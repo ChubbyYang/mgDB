8d5ae548fd37e9bf58cb0f0e23acb36ccc4be00a
test <nnhjgg@163.com>
2017-12-22 15:30:12 +0800

    add mongoose

---
+++ mongodb/db.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Connection URL
+const DB_URL  = 'mongodb://localhost:27017/food';
+
+// Use connect method to connect to the Server
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+db.once('open' ,() => {
+	console.log('连接数据库成功');
+})
+
+db.on('error', function(error) {
+    console.error('Error in MongoDb connection: ' + error);
+    mongoose.disconnect();
+});
+
+db.on('close', function() {
+    console.log('数据库断开，重新连接数据库');
+    mongoose.connect(config.url, {server:{auto_reconnect:true}});
+});
+
+const Schema = mongoose.Schema;
+
+var GoodsSchema = new Schema({          
+    goodsId : { type: Number },                
+    goodsImg: {type: String},            
+    goodsName: {type: String},                   
+    price : { type: Number}                    
+});
+
+module.exports = mongoose.model('Goods',GoodsSchema);
+
